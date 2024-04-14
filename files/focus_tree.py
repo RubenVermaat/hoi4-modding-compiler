8d3f95f6bb4e087e3980627942183fe2055a0af7
Ruben Vermaat <rubenvermaat01@gmail.com>
2024-04-14 19:40:31 +0200

    Worked on code

--- files/focus_tree.py
+++ files/focus_tree.py
@@ -5,13 +5,21 @@ class FocusTree:
         self.focuses = []
         self.id = -1
         self.groups = []
+        self.arguments = []
+        self.continuous_focus_x = 0
+        self.continuous_focus_y = 0
 
+    #Adding information
+    def AddArgument(self, argument):
+        self.arguments.append(argument)
     def AddFocus(self, focus):
         self.focuses.append(focus)
     def SetID(self, id):
         self.id = id
     def AddGroup(self, name, index):
         self.groups.append(Group(name, index))
+
+    #Groups
     def GetLastGroupSubgroup(self, index):
         if len(self.groups) > 0:
             for i in range((len(self.groups)-1), -1, -1):
--- main.py
+++ main.py
@@ -13,35 +13,48 @@ focus = None
 focus_tree = FocusTree()
 indents = 0
 typeRegex = '[^a-zA-Z0-9_]'
-valueRegex = '[^a-zA-Z0-9_]'
+valueRegex = '[^a-zA-Z0-9_.-]'
 commandRegex = '[^a-zA-Z0-9_#]'
 for line in file.readlines():
     if re.sub(commandRegex, '', line).startswith("#") == False:
-        if line.startswith("focus"):
-            tempName = re.sub(typeRegex, '', line[0:line.index(':')])
-            indents = 0
-            focus_tree.AddGroup(tempName, indents)
-        elif line.find(":") != -1:
-            tempName = re.sub(typeRegex, '', line[0:line.index(':')])
-            if focus_tree.groups != []:
-                if line.count("\t") < indents:
+        if line.startswith("continuous_focus_position"):
+            values = line[line.index('(')+1:line.index(')')]
+            focus_tree.continuous_focus_x = values[0:values.index(',')]
+            focus_tree.continuous_focus_y = values[values.index(',')+1:len(values)]
+        else:
+            if line.startswith("focus"):
+                tempName = re.sub(typeRegex, '', line[0:line.index(':')])
+                indents = 0
+                focus_tree.AddGroup(tempName, indents)   
+            elif line.find(":") != -1:
+                #Its a group
+                tempName = re.sub(typeRegex, '', line[0:line.index(':')])
+                if focus_tree.groups != []:
+                    if line.count("\t") < indents:
+                        indents = line.count("\t")
+                        focus_tree.GetLastGroup().AddGroup(tempName, indents)
+                    elif line.count("\t") > indents: #Group in a group
+                        focus_tree.GetLastGroupSubgroup(indents).AddGroup(tempName, (indents+1))
+                    elif line.count("\t") == indents:
+                        focus_tree.GetIndexLastGroup((indents-1)).AddGroup(tempName, indents)
+            elif line.find("=") != -1:
+                #Its a single argument and value
+                if (line.count("\t") != 0):
+                    type = re.sub(typeRegex, '', line[0:line.index('=')])
+                    value = re.sub(valueRegex, '', line[line.index('=')+1:len(line)])
                     indents = line.count("\t")
-                    focus_tree.GetLastGroup().AddGroup(tempName, indents)
-                elif line.count("\t") > indents: #Group in a group
-                    focus_tree.GetLastGroupSubgroup(indents).AddGroup(tempName, (indents+1))
-                elif line.count("\t") == indents:
-                    focus_tree.GetIndexLastGroup((indents-1)).AddGroup(tempName, indents)
-        elif line.find("=") != -1:
-            type = re.sub(typeRegex, '', line[0:line.index('=')])
-            value = re.sub(valueRegex, '', line[line.index('=')+1:len(line)])
-            indents = line.count("\t")
-            focus_tree.GetIndexLastGroup((indents-1)).AddArgument(Argument(type, value))
-        elif len(re.sub(valueRegex, '', line)) > 0:
-            type = "SingleArgument"
-            value = re.sub(valueRegex, '', line)
-            indents = line.count("\t")
-                
-            focus_tree.GetIndexLastGroup((indents-1)).AddArgument(Argument(type, value))
+                    focus_tree.GetIndexLastGroup((indents-1)).AddArgument(Argument(type, value))
+                else:
+                    type = re.sub(typeRegex, '', line[0:line.index('=')])
+                    value = re.sub(valueRegex, '', line[line.index('=')+1:len(line)])
+                    focus_tree.AddArgument(Argument(type, value))
+            elif len(re.sub(valueRegex, '', line)) > 0:
+                #Its a single value
+                type = "SingeValue"
+                value = re.sub(valueRegex, '', line)
+                indents = line.count("\t")
+                    
+                focus_tree.GetIndexLastGroup((indents-1)).AddArgument(Argument(type, value))
                 #focus_tree.GetLastGroup().AddArgument(Argument(type, value))
     else: 
         if focus != None:
@@ -68,7 +81,7 @@ def recursive_loop(f, item):
 def WriteArgument(argument):
     if argument.type == "prerequisite":
         return argument.type + " { focus = " + argument.value + " }"
-    elif argument.type == "SingleArgument":
+    elif argument.type == "SingeValue":
         return argument.value
     else:
         return argument.type + " = " + argument.value
@@ -76,6 +89,12 @@ def WriteArgument(argument):
 file_path = 'example.txt'
 os.remove(file_path)
 with open(file_path, 'w') as f:
+    f.write("continuous_focus_position = {\n")
+    f.write("\tx = " + focus_tree.continuous_focus_x + "\n")
+    f.write("\ty = " + focus_tree.continuous_focus_y + "\n")
+    f.write("}\n")
+    for argument in focus_tree.arguments:
+        f.write(WriteArgument(argument) + "\n")
     for group in focus_tree.groups:
         recursive_loop(f, group)
 
